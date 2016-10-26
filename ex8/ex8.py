d822ed0462df41426a34974b0237865db589487e
Simranjit Singh <simranjit.singh@cuelogic.com>
2016-10-26 19:24:38 +0530

    Python-learning-Hardway-Ex8

---
+++ ex8/ex8.py
@@ -0,0 +1,13 @@
+print "++++++++++Exercise 8: Printing, Printing++++++++++"
+
+formatter = "%r %r %r %r"
+
+print formatter % (1, 2, 3, 4)
+print formatter %('one', 'two', 'three', 'four')
+print formatter % (True, False, True, False)
+print formatter % (
+  'I havd this thing',
+  'That you could type up right',
+  "But it didn't sing",
+  'So I said goodnight'
+  )
