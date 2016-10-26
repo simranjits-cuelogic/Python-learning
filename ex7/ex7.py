c978eca877496b6490bf05c890a5b966da8d66dd
Simranjit Singh <simranjit.singh@cuelogic.com>
2016-10-26 19:23:30 +0530

    Python-learning-Hardway-Ex5

---
+++ ex7/ex7.py
@@ -0,0 +1,33 @@
+print "++++++++++Exercise 7: More Printing++++++++++"
+
+print "Mary had a little lamb."
+print "Its fleece was white as %s." % 'show'
+print "And everywhere that Mary went."
+# like it
+print "_-" * 30 #what'd that do?
+
+end1 = "C"
+end2 = "h"
+end3 = "e"
+end4 = "e"
+
+end5 = "s"
+end6 = "e"
+end7 = "B"
+end8 = "u"
+
+end9 = "r"
+end10 = "g"
+end11 = "e"
+end12 = "r"
+
+#watch that comma at the end. try to
+# removing it and see what happens
+
+print end1 + end2 + end3 + end4 + end5 + end6,
+print end7 + end8 + end10 + end11 + end12
+
+
+# if you have comma in end of the print
+# then next pring will show its result after placing whitespace at same line
+# and if you remove comma(,) next print will show result in new line.
---
+++ ex7/ex7_study_drill.py
@@ -0,0 +1,39 @@
+print "++++++++++Exercise 7: More Printing++++++++++"
+
+# executing simple print statement
+print "Mary had a little lamb."
+
+# printing string
+print "Its fleece was white as %s." % 'show'
+
+# executing simple print statement
+print "And everywhere that Mary went."
+# like it
+print "_-" * 30 #what'd that do?
+
+# assigning varriables
+end1 = "C"
+end2 = "h"
+end3 = "e"
+end4 = "e"
+
+end5 = "s"
+end6 = "e"
+end7 = "B"
+end8 = "u"
+
+end9 = "r"
+end10 = "g"
+end11 = "e"
+end12 = "r"
+
+#watch that comma at the end. try to
+# removing it and see what happens
+
+# printing all variables all together and do some tricks
+print end1 + end2 + end3 + end4 + end5 + end6,
+print end7 + end8 + end10 + end11 + end12
+
+# if you have comma in end of the print
+# then next pring will show its result after placing whitespace at same line
+# and if you remove comma(,) next print will show result in new line.
