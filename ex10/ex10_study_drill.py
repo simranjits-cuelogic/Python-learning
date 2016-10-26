10c1a41267cccffb32d2f5425e8a2f35bd509ad5
Simranjit Singh <simranjit.singh@cuelogic.com>
2016-10-26 19:25:42 +0530

    Python-learning-Hardway-Ex10

---
+++ ex10/ex10_study_drill.py
@@ -0,0 +1,42 @@
+print "++++++++++Exercise 10: What Was That?++++++++++"
+
+tabby_cat = "\tI'm tabbed in."
+persian_cat = "I'm split\non a line"
+backslash_cat = "I'm \\a \\ cat."
+
+fat_cat = '''
+I'll do at list.
+\t* Cat food
+\t* Fishies
+\t* Catnip\n\t* Grass
+
+'''
+print tabby_cat
+print persian_cat
+print backslash_cat
+print fat_cat
+
+
+"""
+\\        Backslash(\)
+\'        Backslash(')
+\"        Backslash(")
+\a        ASCII bell (BEL)
+\b        ASCII backspace (BS)
+\f        ASCII formfeed (FF)
+\n        ASCII linefeed (LF)
+\N{name}  Character named name in the Unicode database (Unicode only)
+\r        Carrige Return (CR)
+\t        Horixontal Tab (TAB)
+\uxxxx    Character with 16-bit hex value xxxx(u'' string only)
+\Uxxxx    Character with 32-bit hex value xxxxxxxx(u'' string only)
+\v        ASCUU vertical tab (VT)
+\ooo      Character with octoal value ooo
+\hh      Character with hex value hh
+
+"""
+
+
+
+
+
