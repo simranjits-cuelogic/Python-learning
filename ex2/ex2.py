dbe0e40638971d040e233e1f2a9bf426ffa3e390
Simranjit Singh <simranjit.singh@cuelogic.com>
2016-10-26 19:18:14 +0530

    Python-learning-Hardway-Ex2

---
+++ ex2/ex2.py
@@ -0,0 +1,8 @@
+print "Hello Pytohon!"
+print "I said hello!!"
+print "I would like to say thanks to you."
+print "I wouldn't (would not) let you down!"
+print "I wouldn\'t (would not) let you down!"
+print "This way you can print back slash\\"
+
+print "I \"said\", don't call me EVER"
