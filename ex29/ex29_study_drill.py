1885dd1b824b648d9c9d1d01f8746684fd1b5a4e
Simranjit Singh <simranjit.singh@cuelogic.com>
2016-11-02 12:39:54 +0530

    Ex about if condition and boolean decision making

---
+++ ex29/ex29_study_drill.py
@@ -0,0 +1,23 @@
+print "++++++++++Exercise 29: What If++++++++++"
+
+people = 20
+cats = 30
+dogs = 15
+
+if True:
+  print 'If condition with True'
+
+if False:
+  print 'If condition with False. will not print this line'
+
+if not False:
+  print 'If condition with False along leading not. will print this line'
+
+if True or False:
+  print 'If condition with \'or\'. will print this line'
+
+if not True or False:
+  print 'If condition with \'or\' along leading not. will not print this line'
+
+if not ("testing" == "testing" and "Zed" == "Cool Guy"):
+  print 'If condition along leading not. will print this line'
