8e15e30337d7b2c5fa3d707f46bd7bfebb0212c4
Simranjit Singh <simranjit.singh@cuelogic.com>
2016-11-02 11:38:32 +0530

    Ex about logic, truth tables

---
+++ ex27/ex27.py
@@ -0,0 +1,44 @@
+print "++++++++++Exercise 27: Memorizing Logic++++++++++"
+# The Truth Tables
+
+"""
+NOT         True?
+not False   True
+not True    False
+
+OR              True?
+True or False   True
+True or True    True
+False or True   True
+False or False  False
+
+AND             True?
+True or False   False
+True or True    True
+False or True   False
+False or False  False
+
+NOT OR         True?
+not (True or False )  False
+not (True or True)    False
+not (False or True)   False
+not (False or False)  True
+
+NOT AND         True?
+not (True or False )  True
+not (True or True)    False
+not (False or True)   True
+not (False or False)  True
+
+!=                    True?
+1 != 0                False
+1 != 1                True
+0 != 0                True
+0 != 0                False
+
+==                    True?
+1 == 0                False
+1 == 1                True
+0 == 0                False
+0 == 0                True
+"""
